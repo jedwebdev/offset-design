d56304a0e54bbb2083a01c08f66a948d81352280
jed208 <jed208@protonamil.com>
2021-01-11 14:15:57 -0700

    updated for mobile

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menuToggle = document.querySelector('.menuToggle');
+  const navigation = document.querySelector('.navigation');
+  menuToggle.classList.toggle('active')
+  navigation.classList.toggle('active')
+}
