d02fc7f467a389577c7604454d0c3716622ecc20
jed208 <jed208@protonamil.com>
2021-02-05 12:12:39 -0700

    updated main nav and added gallery image display fix

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('header');
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
 function toggleMenu() {
   const menuToggle = document.querySelector('.menuToggle');
   const navigation = document.querySelector('.navigation');
